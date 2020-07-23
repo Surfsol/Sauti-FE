c4bfe9747225a25ccbdf17b739e79c340fb51dfb
Surfsol <surfsol@protonamil.com>
2020-07-23 09:55:59 -0300

    update orderedGraphLabels.js

--- src/Components/orderedGraphLabels.js
+++ src/Components/orderedGraphLabels.js
@@ -9,19 +9,19 @@ const ordered = [
   "Preferred Language",
   "Cross Border Trade as Primary Income",
   "Grow/Produce their Own Goods",
-  "INFORMATION INSIGHTS",
-  "Requested Agencies",
-  "Requested Documents",
-  "Requested Procedures, by Commodity",
-  "Requested Procedures, by Commodity Category",
-  "Requested Procedures, by Destination",
   "BUSINESS INSIGHTS",
   "Currency Exchanges",
   "Traders' Destination Country",
   "Traders' Destination Market",
   "Traded Commodities' Origin",
   "Traded Commodities",
-  "Traded Commodity Categories"
+  "Traded Commodity Categories",
+  "INFORMATION INSIGHTS",
+  "Requested Agencies",
+  "Requested Documents",
+  "Requested Procedures, by Commodity",
+  "Requested Procedures, by Commodity Category",
+  "Requested Procedures, by Destination"
 ];
 const demographics = [
   "Age",
