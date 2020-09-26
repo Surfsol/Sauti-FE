1ac2f4d444590dad01bc645361e8a22f53997ae7
Surfsol <surfsol@protonamil.com>
2020-09-26 14:36:27 -0300

    call graphLabels from app.js

--- src/dashboard/App.js
+++ src/dashboard/App.js
@@ -1,8 +1,22 @@
 import React from "react";
-import ReactGA from "react-ga";
 import DashNavBar from "./DashNavBar";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import { catLabelsAction } from "../Components/redux-actions/catLabels";
+import { useDispatch } from "react-redux";
+
+const GET_GRAPHLABELS = gql`
+  query Labels {
+    getGraphLabels
+  }
+`;
 
 function App() {
+  let { data, loading, error } = useQuery(GET_GRAPHLABELS);
+
+  const dispatch = useDispatch();
+  dispatch(catLabelsAction(data));
+
   return (
     <div>
       <DashNavBar />
